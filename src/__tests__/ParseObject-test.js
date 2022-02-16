813ee58ebfa176c062be6a927704deccb38a3910
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2022-02-16 13:50:02 +0300

    Expanded an existing test to reproduce #1452

--- src/__tests__/ParseObject-test.js
+++ src/__tests__/ParseObject-test.js
@@ -660,6 +660,14 @@ describe('ParseObject', () => {
       'objectField.number': 20,
       otherField: { hello: 'world' },
     });
+    expect(o.toJSON()).toEqual({
+      objectField: {
+        number: 20,
+        letter: 'a',
+      },
+      otherField: { hello: 'world' },
+      objectId: 'setNested',
+    });
   });
 
   it('can set multiple nested fields (regression test for #1450)', () => {
